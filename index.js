445b8549c189eb9017c8d7b3098703867e855f9e
SeanyCarol <seany@ice.ufjf.br>
2021-02-16 15:46:13 -0300

    Adiciona acesso a arquivos estáticos

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const connect = require("connect");
+const serveStatic = require("serve-static");
+
 const app = connect();
 app.listen(3000);
+app.use(serveStatic("./public"));
+app.use(static);
 app.use("/index.html", index);
 app.use("/sobre.html", sobre);
 
-function index(req, res, next) {
+function static(req, res, next) {
     res.setHeader("Content-type", "text/html");
+    res.write("<link rel=stylesheet href=estilo.css />")
+    next();
+}
+
+function index(req, res, next) {
     res.end("<h1>Bem vindo!</h1>");
 }
 
 function sobre(req, res, next) {
-    res.setHeader("Content-type", "text/html");
     res.end(` 
-    <label>nome completo: Seany Caroliny Oliveira Silva</label></br>
-    <label>curso: Ciência da Computação</label></br>
-    <label>email de contato: seany.caroliny@estudante.ufjf.br</label> </br>
-    <label>foto de perfil: </label> <img src="url" alt="foto de perfil">   
+    <img src="fotoPerfil.jpg" alt="foto de perfil">  
+    <div class="label">
+        <label>nome completo: Seany Caroliny Oliveira Silva</label></br>
+        <label>curso: Ciência da Computação</label></br>
+        <label>email de contato: seany.caroliny@estudante.ufjf.br</label>
+    </div>
     `);
 }
 
