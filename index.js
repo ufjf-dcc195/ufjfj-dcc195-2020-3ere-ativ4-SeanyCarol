3a1c7892a6dd66255877888229e5582945393ede
SeanyCarol <seany@ice.ufjf.br>
2021-02-16 15:26:59 -0300

    Cria middleware em connect

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const connect = require("connect");
 const app = connect();
 app.listen(3000);
+app.use("/index.html", index);
+
+function index(req, res, next) {
+    res.setHeader("Content-type", "text/html");
+    res.end("<h1>Bem vindo!</h1>");
+}
 
 console.log("Servidor escutando em http://localhost:3000");
